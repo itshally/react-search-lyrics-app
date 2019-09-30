0f99a294d1144302516f294300a6e1e1c3124b1b
HB <hbianan@gmail.com>
2019-09-29 21:09:51 -0400

    files updated
    -added some files

---
+++ src/context.js
@@ -0,0 +1,33 @@
+import 'dotenv'
+import React, { Component } from 'react'
+import axios from 'axios';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+    state = { 
+        track_list: [],
+        heading: 'Top 10 Tracks'
+    }
+
+    componentDidMount(){
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=ca&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+             .then( result => {
+                    // console.log(result.data)
+                    this.setState({
+                        track_list: result.data.message.body.track_list
+                    })
+              })
+             .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export const Consumer = Context.Consumer;
