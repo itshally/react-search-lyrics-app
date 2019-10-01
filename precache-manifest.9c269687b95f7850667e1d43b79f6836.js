20c7989e5e30b53b26996c3e7311e630d554e886
HB <hbianan@gmail.com>
2019-10-01 19:06:47 -0400

    Updates

---
+++ precache-manifest.9c269687b95f7850667e1d43b79f6836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0447f05eaf221f8d9459921e811a66",
+    "url": "/react-search-lyrics-app/index.html"
+  },
+  {
+    "revision": "11be61a06e68cff97168",
+    "url": "/react-search-lyrics-app/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "e8dd26b0c12b58bd19a1",
+    "url": "/react-search-lyrics-app/static/js/2.e021c6dd.chunk.js"
+  },
+  {
+    "revision": "11be61a06e68cff97168",
+    "url": "/react-search-lyrics-app/static/js/main.47702eb3.chunk.js"
+  },
+  {
+    "revision": "53377aac8dc291d71a2e",
+    "url": "/react-search-lyrics-app/static/js/runtime-main.2486c338.js"
+  }
+]);
