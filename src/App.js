97e1889f88eead5fd34eb7e42b84be35eddcce47
HB <hbianan@gmail.com>
2019-10-01 21:34:05 -0400

    files updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import Lyrics from './components/tracks/Lyrics';
+import Footer from './components/layout/Footer';
 import { Container } from 'react-bootstrap';
 import { Provider } from './context';
 
@@ -19,6 +20,7 @@ function App() {
               <Route exact path="/react-search-lyrics-app/lyrics/track/:id" component={Lyrics}/>
             </Switch>
           </Container>
+          <Footer/>
         </React.Fragment>
       </Router>
     </Provider>
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="container-fluid footer-container">
+            <p>&copy; 2019 All Rights Reserved. &nbsp;	
+                <a href="https://github.com/itshally">Hally</a>. &nbsp;	
+                Powered by <a href="https://github.com">Github</a>
+            </p>
+            <p>Credits to <a href="https://www.youtube.com/playlist?list=PLillGF-RfqbaxgxkKgKk1XlJAVCX31xRI">Traversy Media</a></p>
+        </div>
+    )
+}
+
+export default Footer;
